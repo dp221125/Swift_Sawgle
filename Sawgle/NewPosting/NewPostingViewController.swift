db4721a7810f7ed69fb4fc243890b2351237a0d2
Eddie <eahnivy8@gmail.com>
2019-06-18 22:33:31 +0900

    Add NewPosting SB and VC

---
+++ Sawgle/NewPosting/NewPostingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NewPostingViewController.swift
+//  Sawgle
+//
+//  Created by Eddie Ahn on 18/06/2019.
+//  Copyright © 2019 Meo MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class NewPostingViewController: UIViewController {
+
+    @IBOutlet weak var postingView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        postingView.backgroundColor = .white
+        postingView.layer.cornerRadius = 30
+    }
+    
+
+
+
+}
